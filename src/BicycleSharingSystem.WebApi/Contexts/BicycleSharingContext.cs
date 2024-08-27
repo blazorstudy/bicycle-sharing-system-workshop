e8e4bb4d61b5ea1a9c5ffe4c84c126aefb686f60
Gusam Park <tnara9573@gmail.com>
2024-08-28 00:26:26 +0900

    Create solution and update readme

---
+++ src/BicycleSharingSystem.WebApi/Contexts/BicycleSharingContext.cs
@@ -0,0 +1,32 @@
+﻿using BicycleSharingSystem.WebApi.Contexts.Bases;
+using BicycleSharingSystem.WebApi.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BicycleSharingSystem.WebApi.Contexts;
+
+/// <summary>
+/// Bicycle Sharing Context.
+/// </summary>
+public sealed class BicycleSharingContext : DatabaseContextBase
+{
+    private const string DbFileName = "bicycle_sharing.db";
+
+    /// <summary>
+    /// Initialize <see cref="BicycleSharingContext"/> Instance.
+    /// </summary>
+    public BicycleSharingContext() : base(DbFileName)
+    {
+        Database.EnsureCreated();
+    }
+
+    /// <summary>
+    /// Rental Offices
+    /// </summary>
+    public DbSet<RentalOfficeModel> RentalOffices { get; init; }
+
+    /// <summary>
+    /// Bicycles.
+    /// </summary>
+    public DbSet<BicycleModel> Bicycles { get; init; }
+}
---
+++ src/BicycleSharingSystem.WebApi/Program.cs
@@ -0,0 +1,40 @@
+using BicycleSharingSystem.WebApi.Contexts;
+
+namespace BicycleSharingSystem.WebApi;
+
+/// <summary>
+/// Program Entrypoint Support.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// Program Entrypoint.
+    /// </summary>
+    /// <param name="args">args</param>
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<BicycleSharingContext>();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
