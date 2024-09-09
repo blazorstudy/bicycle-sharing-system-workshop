64162ff18a814821c1c7e0e4edf97d10807d0831
lukewire129 <lukewire129@gmail.com>
2024-09-09 17:28:56 +0900

    - 전체 틀 나눔 작업  - Navigation Command 이벤트 연동

--- src/BicycleSharingSystem.Kiosk/ViewModels/MainWindowViewModel.cs
+++ src/BicycleSharingSystem.Kiosk/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,26 @@
-﻿namespace BicycleSharingSystem.Kiosk.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BicycleSharingSystem.Kiosk.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+    public MainWindowViewModel()
+    {
+    }
+
+    [RelayCommand]
+    private void HomeButton()
+    {
+        Console.WriteLine(Greeting);
+    }
+    [RelayCommand]
+    private void RentalOfficeButton()
+    {
+        Console.WriteLine(Greeting);
+    }
 }
